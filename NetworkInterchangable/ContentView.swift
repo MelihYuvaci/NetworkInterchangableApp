af40123c5eed11c5df82f211db9e5480640b9262
MelihYuvaci <melihyuvaci@icloud.com>
2022-08-25 11:08:03 +0300

    Initial Commit

---
+++ NetworkInterchangable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkInterchangable
+//
+//  Created by Melih Yuvacı on 25.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by Melih Yuvacı on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
